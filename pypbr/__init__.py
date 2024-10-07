7acecbb88dc3a533d6a84eabbd65818ba3eb7d6a
Giuseppe Vecchio <giuseppevecchio@hotmail.com>
2024-10-07 14:13:46 +0200

    Replace setup.py with pyproject.toml

--- pypbr/__init__.py
+++ pypbr/__init__.py
@@ -1,16 +1,38 @@
-from .io import load_material_from_folder, save_material_to_folder
-from .material import MaterialBase, BasecolorMetallicMaterial, DiffuseSpecularMaterial
+"""
+pypbr.transforms
+
+This module provides transformations for PBR materials, similar to torchvision's transforms.
+It includes both functional APIs and transformation classes that can be composed.
+
+Modules:
+    io: Input/output functions for saving and loading materials.
+    material: Classes representing PBR materials.
+    models: Classes representing BRDF models.
+    utils: Utility functions for color space conversion.
+
+Classes:
+    MaterialBase,
+    BasecolorMetallicMaterial,
+    DiffuseSpecularMaterial,
+    BRDFModel,
+    CookTorranceBRDF,
+
+Functions:
+    load_material_from_folder,
+    save_material_to_folder,
+    linear_to_srgb,
+    srgb_to_linear,
+"""
+
+from . import io, utils
+from ._version import version as __version__
+from .material import BasecolorMetallicMaterial, DiffuseSpecularMaterial, MaterialBase
 from .models import BRDFModel, CookTorranceBRDF
-from .utils import linear_to_srgb, srgb_to_linear
 
 __all__ = [
     "MaterialBase",
     "BasecolorMetallicMaterial",
     "DiffuseSpecularMaterial",
-    "load_material_from_folder",
-    "save_material_to_folder",
     "BRDFModel",
     "CookTorranceBRDF",
-    "srgb_to_linear",
-    "linear_to_srgb",
 ]
